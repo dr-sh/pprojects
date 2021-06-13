1bf7b479c921814e77de14801cccf4ec7ee26ac3
dr-sh <85134998+dr-sh@users.noreply.github.com>
2021-06-14 02:37:41 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,18 @@
+
+
+function submission(){
+  var name = document.getElementByID = "name";
+  var psswd = document.getElementByID = "psswd";
+  var usr = document.getElementByID = "usr";
+
+  var name = new Array();
+  var usr = new Array();
+  var psswd = new Array();
+  
+  
+ 
+function store()
+{
+Var elename =document.getElementById (‘name’);
+localStorage.setItem(‘name’,elename.value)
+}
